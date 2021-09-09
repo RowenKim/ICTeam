2efe603331b39894ddcd22285bd02370d2ea934f
RowenKim <jw909069@naver.com>
2021-09-09 16:53:32 +0900

    김종원

---
+++ src/main/java/com/lec/spring/GitIcTeamApplication.java
@@ -0,0 +1,13 @@
+package com.lec.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIcTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitIcTeamApplication.class, args);
+	}
+
+}
