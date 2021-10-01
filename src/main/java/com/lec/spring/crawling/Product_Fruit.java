1646708cbf952476aee5d88c8034179e4148a1e4
wjdwodnr5452 <wjdwodnr5452@naver.com>
2021-10-01 13:44:25 +0900

    과일 클래스

---
+++ src/main/java/com/lec/spring/crawling/Product_Fruit.java
@@ -0,0 +1,19 @@
+package com.lec.spring.crawling;
+
+import org.jsoup.select.Elements;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Builder
+@Getter
+public class Product_Fruit {
+	
+	private String pro_title;
+	private String pro_img;
+	private String pro_price;
+
+	
+}
